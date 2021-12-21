9a421737f0e8b5ade4059d449cc484f886787fb8
Chagay123 <94804738+Chagay123@users.noreply.github.com>
2021-12-21 17:03:05 +0300

    Add files via upload

---
+++ lesson3/js/app.js
@@ -0,0 +1,24 @@
+// let a = 10;
+// if (a > 10){
+//     console.log('много');
+// }else if(a==10){
+//     console.log('идеально');
+// }
+// else{
+//     console.log("нормально");
+// }alert
+let a = prompt("первое число", );
+let b = prompt("второе число", );
+let operation = prompt( 'write second value')
+if(operation == '+'){
+console.log(+a + +b);
+}
+else if(operation == '-'){
+    console.log(+a - +b);
+}
+else if(operation == '*'){
+    console.log(+a * +b);
+}
+else if (operation == '/'){
+    console.log(+a / +b);
+}
